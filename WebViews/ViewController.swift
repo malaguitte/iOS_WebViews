7b676d73bc37a27f3bf542815701722a1126de16
Anderson <upmalagutti@gmail.com>
2020-06-18 18:36:00 +0200

    Created a very simple browser app

--- WebViews/ViewController.swift
+++ WebViews/ViewController.swift
@@ -10,32 +10,53 @@ import UIKit
 import SafariServices
 import WebKit
 
-class ViewController: UIViewController, SFSafariViewControllerDelegate {
+class ViewController: UIViewController, SFSafariViewControllerDelegate, UITextFieldDelegate {
     let WEBSITE_ADDRESS = "https://google.com"
 
+    @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        textField.delegate = self
         if let urlObject = URL(string: WEBSITE_ADDRESS) {
             webView.load(URLRequest(url: urlObject))
         }
     }
     
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        print("Finished!")
+  
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //get the text if there's any, otherwise empty string
+        let address = textField.text ?? "";
+        //Fixing the url quickly if needed
+        let adressFormatted = self.getUrlWithHttps(url: address)
+        if let urlObject = URL(string: adressFormatted) {
+            webView.load(URLRequest(url: urlObject))
+        }
+        textField.resignFirstResponder() //hides keyboard
+        return false
     }
-
-
-    @IBAction func didPressButton(_ sender: UIButton) {
-        //Opens safari
-        self.openSafariApp(url: WEBSITE_ADDRESS)
-            
-        //Opens safari view controller (within the current app)
-        //self.openSafariControllerView(url: WEBSITE_ADDRESS)
+    
+    func getUrlWithHttps(url: String) -> String {
+        return url.contains("https://") ? url : "https://\(url)"
     }
+    
+    @IBAction func didPressBackButton(_ sender: UIButton) {
+        webView.goBack()
+    }
+    
+    /*
+       @IBAction func didPressButton(_ sender: UIButton) {
+           //Opens safari
+           self.openSafariApp(url: WEBSITE_ADDRESS)
+               
+           //Opens safari view controller (within the current app)
+           //self.openSafariControllerView(url: WEBSITE_ADDRESS)
+       }
+    */
     
     func openSafariApp(url: String) {
         if let urlObject = URL(string: url) {
@@ -50,5 +71,10 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate {
             present(safariViewController, animated: true, completion: nil)
         }
     }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+          print("Finished!")
+      }
+
 }
 
